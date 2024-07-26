7f77fbcfb41c49bececa3c7353ee86501c278c77
incrisz <incrisz4luv@gmail.com>
2024-07-26 05:39:14 +0100

    mysql workbench

---
+++ mysqlworkbench_on_parrot.sh
@@ -0,0 +1,8 @@
+# If the above steps fail, you can try installing MySQL Workbench using Snap.
+
+# Install Snapd:
+sudo apt update
+sudo apt install snapd
+
+# Install MySQL Workbench via Snap:
+sudo snap install mysql-workbench-community
