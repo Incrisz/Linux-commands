f203b21c85039a210c10dc611e52e1ffad59930b
incrisz <incrisz4luv@gmail.com>
2024-08-25 05:19:48 +0100

    hh

---
+++ Autofixer/mail-server.sh
@@ -0,0 +1,16 @@
+# https://zacs-tech.com/how-to-install-and-configure-mail-server-in-ubuntu-22-04-easily/
+
+
+
+# Modoboa
+
+
+sudo ./run.py --stop-after-configfile-check elbethelacademy.com
+
+sudo hostnamectl set-hostname mail.elbethelacademy.com
+
+sudo ./run.py --interactive elbethelacademy.com
+
+
+sudo ./run.py --interactive  --debug elbethelacademy.com
+
---
+++ ssh/update-ssh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the paths to the files
+FILE_TO_REMOVE="/etc/ssh/sshd_config.d/60-cloudimg-settings.conf"
+NEW_FILE="/etc/ssh/sshd_config.d/ssh.conf"
+
+# Use sudo to ensure we have the necessary permissions
+# Remove the existing configuration file (if it exists)
+sudo rm -f "${FILE_TO_REMOVE}"
+
+# Create a new SSH configuration file with the desired settings
+echo "Port 22
+PermitRootLogin yes
+PasswordAuthentication yes" | sudo tee "${NEW_FILE}" > /dev/null
+
+# Restart SSH service to apply changes
+sudo systemctl restart ssh
+
+echo "I'm done updating ssh configuration...all thanks to Incrisz"
+
+
+# chmod +x update_ssh_config.sh
