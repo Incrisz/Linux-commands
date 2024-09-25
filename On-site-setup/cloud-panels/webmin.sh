1947d419943d25d3fd3595cbb25c12dbe213027c
incrisz <incrisz4luv@gmail.com>
2024-09-25 11:22:48 +0100

    k

--- On-site-setup/cloud-panels/webmin.sh
+++ On-site-setup/cloud-panels/webmin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the fully qualified hostname
-sudo hostnamectl set-hostname elbethelacademy.com
+sudo hostnamectl set-hostname webmin.cyfamod.com
 
 # Update the package list
 sudo apt update
@@ -17,7 +17,7 @@ sudo chmod +x install.sh
 
 # Run the Virtualmin installation script with the automatic option
 # The `-y` flag will answer "yes" to all prompts automatically
-sudo /bin/sh install.sh -y --hostname elbethelacademy.com --force --verbose
+sudo /bin/sh install.sh -y --hostname webmin.cyfamod.com --force --verbose
 
 
 
@@ -25,5 +25,5 @@ sudo /bin/sh install.sh -y --hostname elbethelacademy.com --force --verbose
 
 
 
-
+# use user : root and password : {root-password} 
 
