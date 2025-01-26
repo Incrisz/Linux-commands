eb6f19b75305419497f4afdaad5272c78016213c
incrisz <incrisz4luv@gmail.com>
2025-01-26 01:37:23 +0100

    d

--- peernetics/new-docker/1-run.sh
+++ peernetics/new-docker/1-run.sh
@@ -15,3 +15,16 @@ cp .env.template .env
 sed -i 's|^DATABASE_URL=postgresql://postgres:Qwerty.1234@3.11.67.148:5432/postgres_testing| DATABASE_URL=postgresql://postgres:password@postgres-database:5432/database|' .env
 
 docker-compose up -d
+
+
+
+# docker exec -it loyalty-club_web_1 /bin/bash -c "git pull && other_command"
+
+
+#master
+docker exec -it loyalty-club_web_1 /bin/bash -c "git pull"
+docker restart loyalty-club_web_1
+
+#dev 
+docker exec -it loyalty-club-dev_web_1 /bin/bash -c "git add . && git commit -m "update" && git pull"
+docker restart loyalty-club-dev_web_1
